f336cada73ab29e065b1d5f07c96c32ffc990367
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2021-08-08 04:32:15 +0430

    added ranks table and some style to settings part

--- Front/main.js
+++ Front/main.js
@@ -1,5 +1,87 @@
 PD_API_URL = "http://localhost:8082"
 
+window.onload = function () {
+  points = []
+  fetch("http://127.0.0.1:8082/annual-income").then(
+    res => {
+      res.json().then(
+        data => {
+          console.log(data);
+          if (data.length > 0) {
+
+            var temp = "";
+            data.forEach((itemData) => {
+              points.push({ x: new Date(Date.parse(itemData.date)), y: itemData.sale });
+            });
+          }
+          console.log("my data", points);
+          console.log("sample data", points);
+
+          var chart = new CanvasJS.Chart("chartContainer",
+            {
+              title: {
+                text: "Earthquakes - per month"
+              },
+              data: [
+                {
+                  type: "line",
+
+                  dataPoints: points
+                }
+              ]
+            });
+
+          chart.render();
+        }
+      )
+    })
+
+  reload_ranks_table()
+}
+
+function reload_ranks_table() {
+  var request = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+  };
+  fetch(PD_API_URL + "/rank", request).then(function (response) {
+    stat = response.status
+    if (stat == 200) {
+      response.text().then(function (res) {
+        ranks = JSON.parse(res)
+        replace_ranks(ranks)
+      })
+    } else {
+      elem = document.getElementById("danger_alert")
+      elem.style.opacity = "1";
+      elem.style.visibility = "visible";
+      document.getElementById("error_message").innerHTML = response.statusText
+      console.log(response.statusText)
+    }
+  }).catch(function (error) {
+    console.log("Error: " + error);
+  })
+
+}
+
+function replace_ranks(ranks) {
+  ranks_table_html = ""
+  Object.keys(ranks).forEach(function(key) {
+    ranks_table_html += `<tr>
+    <td>${key}</td>
+    <td>${ranks[key]["off"]}</td>
+    <td>${ranks[key]["rank range"][0]}-${ranks[key]["rank range"][1]}</td>
+    <td>${ranks[key]["monthly limit"]}</td>
+    <td>${ranks[key]["free shipping"]}</td>
+    <td><button type="button" class="close" aria-label="Close"><span
+          aria-hidden="true">&times;</span></button></td>
+  </tr>`
+  })
+  document.getElementById("ranks-table-content").innerHTML = ranks_table_html
+}
+
+
+
 function settings_clicked() {
   document.getElementById("settings-layout").style.display = "block";
   document.getElementById("analytics-layout").style.display = "none";
@@ -64,7 +146,7 @@ function submite_add_rank_form() {
   rank_range_max = add_rank_form["rank_range_max_input"].value
   rank_range_min = add_rank_form["rank_range_min_input"].value
   free_shipping = add_rank_form["free_shipping_input"].checked
-  if (!rank_name || !off || !monthly_limit || !rank_range_max || !rank_range_min){
+  if (!rank_name || !off || !monthly_limit || !rank_range_max || !rank_range_min) {
     elem = document.getElementById("danger_alert")
     elem.style.opacity = "1";
     elem.style.visibility = "visible";
@@ -73,7 +155,7 @@ function submite_add_rank_form() {
   }
 
   let data = [`name=${rank_name}`, `off=${off}`, `monthly_limit=${monthly_limit}`, `rank_range=${rank_range_min}_${rank_range_max}`,
-              free_shipping ? `free_shipping=true` : `free_shipping=false`]
+  free_shipping ? `free_shipping=true` : `free_shipping=false`]
   data = data.filter(x => x != "").join("&")
   var request = {
     method: 'POST',
